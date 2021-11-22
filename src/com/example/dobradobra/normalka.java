195eb956af68c689cbdd3efc6d94ca2d62867e30
hendrykik <Hendrykik@wp.pl>
2021-11-22 01:44:37 +0100

    teraz serio

--- src/com/example/dobradobra/normalka.java
+++ src/com/example/dobradobra/normalka.java
@@ -4,5 +4,6 @@ public class normalka {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("serio sie udalo p?");
+        System.out.println("serio spoka go?");
     }
 }
