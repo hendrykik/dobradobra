5ab19f8d451af95be103cbd0fa1aca17c04b105c
hendrykik <Hendrykik@wp.pl>
2021-11-22 01:42:53 +0100

    no dobra ostatni raz

--- src/com/example/dobradobra/normalka.java
+++ src/com/example/dobradobra/normalka.java
@@ -3,5 +3,6 @@ package com.example.dobradobra;
 public class normalka {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("serio sie udalo pogg?");
     }
 }
